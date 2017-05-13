9d6496beedd4a602ef7483c951317ef872bcdd2b
balasubramanyam ganaparthi <balasubramanyamganaparthi@balasubramanyams-MacBook-Pro.local>
2017-05-13 00:01:12 -0500

    added a file and made some changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
    * @param someone is the name of the person
    * @return Greetings to someone
    */
-  public final String greet( final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
